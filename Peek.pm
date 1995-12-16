0bc7a23985915ac50d2bca5cc70ce22e100e90eb
Ilya Zakharevich <cpan@ilyaz.org>
1995-12-16 07:37:00 +0000

    Import of ILYAZ/Devel-Peek-0.5 from CPAN.
    gitpan-cpan-distribution: Devel-Peek
    gitpan-cpan-version:      0.5
    gitpan-cpan-path:         ILYAZ/Devel-Peek-0.5.tar.gz
    gitpan-cpan-author:       ILYAZ
    gitpan-cpan-maturity:     released

---
+++ Peek.pm
@@ -0,0 +1,33 @@
+package Devel::Peek;
+=head1 NAME
+
+Devel::Peek - Shoot yourself in the foot. If you have gone that
+far, you do not need any documentation.
+
+=cut
+
+require Exporter;
+require DynaLoader;
+
+@ISA = qw(Exporter DynaLoader);
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+@EXPORT = qw(
+	
+);
+# Other items we are prepared to export if requested
+@EXPORT_OK = qw(
+		Dump SvREFCNT SvREFCNT_inc SvREFCNT_dec
+);
+%EXPORT_TAGS = ('ALL' => \@EXPORT_OK);
+
+
+bootstrap Devel::Peek;
+
+# Preloaded methods go here.
+
+# Autoload methods go after __END__, and are processed by the autosplit program.
+
+1;
+__END__
---
+++ test.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+
+use Devel::Peek qw(:ALL);
+
+$x = [0, 1, {'two' => 22, 2 => 222}, [3, "three"]];
+
+Dump $x;
+print STDERR "Refcount of \$x is ", SvREFCNT($x), "\n";
+SvREFCNT_inc($x);
+print STDERR "After increment refcount of \$x is ", SvREFCNT($x), "\n";
+SvREFCNT_dec($x);
+print STDERR "After decrement refcount of \$x is ", SvREFCNT($x), "\n";
+
+print STDERR "Refcount of \$x is ", SvREFCNT($x), "\n";
+print STDERR "After increment refcount of \$x is ", SvREFCNT(SvREFCNT_inc($x)), "\n";
+print STDERR "After previous increment refcount of \$x still is ", SvREFCNT($x), "\n";
+print STDERR "After decrement refcount of \$x is ", SvREFCNT(SvREFCNT_dec($x)), "\n";
+print STDERR "After previous decrement refcount of \$x still is ", SvREFCNT($x), "\n";
+my $sub = sub {'aaa'};
+$closure = sub {$sub};
+sub subr {1}
+sub closure {$sub}
+sub prototyped (&@) {'aha'}
+Dump($sub);
+Dump($closure);
+Dump(\&subr);
+Dump(\&closure);
+Dump(\*closure);
+Dump(\&prototyped);
+Dump ($x, 10);
